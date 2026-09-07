aface043db8e970a999163b45f285f938409bd27
Dataanalyst-11 <mis.philmed01.dpt@gmail.com>
2026-09-07 13:36:34 +0300

    Add files via upload

---
+++ beibora.js
@@ -0,0 +1,9 @@
+// Central configuration 
+// beibora.js (Beibora Configuration)
+const BEIBORA_CONFIG = {
+  API_URL: "https://api.beibora.co.ke/invoice-tracker/order-tracking/", 
+  API_TOKEN: "jZYyPDJG1oZjAHhgFJPfy2fl8" // No "Bearer" prefix needed
+};
+
+// Expose it globally
+window.BEIBORA_CONFIG = BEIBORA_CONFIG;
