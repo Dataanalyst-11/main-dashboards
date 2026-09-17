670ea87327d4cf8e443805f6416f49cbad0bd370
Dataanalyst-11 <mis.philmed01.dpt@gmail.com>
2026-09-17 11:16:37 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+// config.js
+// Central configuration for all dashboards
+
+const API_CONFIG = {
+  API_URL: "https://api.philmedltd.com/invoice-tracker/",
+  API_TOKEN: "7YuL2xXAILBLthXPIHtbE2DtawYkf39vdfdi5hBJjhlsnD0UXz"
+};
+
+// Expose it globally for all dashboards
+window.API_CONFIG = API_CONFIG;
+
